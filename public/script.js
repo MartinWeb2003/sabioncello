a7028a522f163825bfbaf14c4b3772d162bc1c3e
MartinWeb2003 <144782669+MartinWeb2003@users.noreply.github.com>
2026-01-28 11:16:51 +0100

    form fix

--- public/script.js
+++ public/script.js
@@ -813,7 +813,7 @@ const translations = {
         submitBtn.style.opacity = "0.7";
       }
 
-      const res = await fetch("http://localhost:3000/api/contact", {
+      const res = await fetch("/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...data, website })
